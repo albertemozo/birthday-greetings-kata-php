3c80b87d4d9ec7cc13a684b054d64fdda74e6267
Alberte Mozo <alberte.mozo@proton.me>
2023-09-14 23:57:39 +0200

    Extract greeter class

---
+++ src/Adapters/EmailGreeter.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BirthdayGreetingsKata\Adapters;
+
+use BirthdayGreetingsKata\Application\Employee;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+
+final class EmailGreeter
+{
+    public function greet(Employee $employee, string $smtpHost, int $smtpPort): void
+    {
+        $mailer = new Mailer(
+            Transport::fromDsn('smtp://' . $smtpHost . ':' . $smtpPort)
+        );
+
+        $message = (new Email())
+            ->subject('Happy Birthday!')
+            ->from('sender@here.com')
+            ->to($employee->getEmail())
+            ->text(sprintf('Happy Birthday, dear %s!', $employee->getFirstName()));
+
+        $mailer->send($message);
+    }
+}
--- src/Application/BirthdayService.php
+++ src/Application/BirthdayService.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace BirthdayGreetingsKata\Application;
 
-use Symfony\Component\Mailer\Mailer;
-use Symfony\Component\Mailer\Transport;
-use Symfony\Component\Mime\Email;
+use BirthdayGreetingsKata\Adapters\EmailGreeter;
 
 final readonly class BirthdayService
 {
+    private EmailGreeter $greeter;
+
     public function __construct(private EmployeesRepository $employeesRepository)
     {
+        $this->greeter = new EmailGreeter();
     }
 
     public function sendGreetings(XDate $xDate, string $smtpHost, int $smtpPort): void
@@ -19,22 +20,7 @@ final readonly class BirthdayService
         $employees = $this->employeesRepository->onBirthday($xDate);
 
         foreach ($employees as $employee) {
-            $this->greet($employee, $smtpHost, $smtpPort);
+            $this->greeter->greet($employee, $smtpHost, $smtpPort);
         }
-    }
-
-    private function greet(Employee $employee, string $smtpHost, int $smtpPort): void
-    {
-        $mailer = new Mailer(
-            Transport::fromDsn('smtp://' . $smtpHost . ':' . $smtpPort)
-        );
-
-        $message = (new Email())
-            ->subject('Happy Birthday!')
-            ->from('sender@here.com')
-            ->to($employee->getEmail())
-            ->text(sprintf('Happy Birthday, dear %s!', $employee->getFirstName()));
-
-        $mailer->send($message);
     }
 }
