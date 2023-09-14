3d3249cae6e8d2ace55bfcb1913b86c153f45f69
Alberte Mozo <alberte.mozo@proton.me>
2023-09-14 23:02:17 +0200

    Use new repository constructor

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -14,11 +14,11 @@ final class BirthdayService
 
     public function __construct()
     {
-        $this->csvEmployeesRepository = new CsvEmployeesRepository();
     }
 
     public function sendGreetings(string $fileName, XDate $xDate, string $smtpHost, int $smtpPort): void
     {
+        $this->csvEmployeesRepository = new CsvEmployeesRepository($fileName);
         $employees = $this->csvEmployeesRepository->employeesOnBirthday($fileName, $xDate);
 
         foreach ($employees as $employee) {
