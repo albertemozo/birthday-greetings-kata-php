395a753b6d67a29c9a39408f346ffc1514231eff
Alberte Mozo <alberte.mozo@proton.me>
2023-09-14 22:54:45 +0200

    Extract repository class

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -10,9 +10,16 @@ use Symfony\Component\Mime\Email;
 
 final class BirthdayService
 {
+    private CsvEmployeesRepository $csvEmployeesRepository;
+
+    public function __construct()
+    {
+        $this->csvEmployeesRepository = new CsvEmployeesRepository();
+    }
+
     public function sendGreetings(string $fileName, XDate $xDate, string $smtpHost, int $smtpPort): void
     {
-        $employees = $this->employeesOnBirthday($fileName, $xDate);
+        $employees = $this->csvEmployeesRepository->employeesOnBirthday($fileName, $xDate);
 
         foreach ($employees as $employee) {
             $recipient = $employee->getEmail();
@@ -40,20 +47,4 @@ final class BirthdayService
         $mailer->send($msg);
     }
 
-    private function employeesOnBirthday(string $fileName, XDate $xDate): array
-    {
-        $fileHandler = fopen($fileName, 'rb');
-        fgetcsv($fileHandler);
-
-        $employees = [];
-
-        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
-            $employeeData = array_map('trim', $employeeData);
-            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-            if ($employee->isBirthday($xDate)) {
-                $employees[] = $employee;
-            }
-        }
-        return $employees;
-    }
 }
---
+++ src/CsvEmployeesRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BirthdayGreetingsKata;
+
+final class CsvEmployeesRepository
+{
+    public function employeesOnBirthday(string $fileName, XDate $xDate): array
+    {
+        $fileHandler = fopen($fileName, 'rb');
+        fgetcsv($fileHandler);
+
+        $employees = [];
+
+        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
+            $employeeData = array_map('trim', $employeeData);
+            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+            if ($employee->isBirthday($xDate)) {
+                $employees[] = $employee;
+            }
+        }
+        return $employees;
+    }
+}
