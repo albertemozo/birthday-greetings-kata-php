68aff49b8eb96aab9446e7dd120b2eb341c225e0
Alberte Mozo <alberte.mozo@proton.me>
2023-09-26 22:36:25 +0200

    Introduce optional greeter on construction

--- src/Application/BirthdayService.php
+++ src/Application/BirthdayService.php
@@ -10,15 +10,16 @@ final readonly class BirthdayService
 {
     private EmailGreeter $greeter;
 
-    public function __construct(private EmployeesRepository $employeesRepository)
+    public function __construct(
+        private EmployeesRepository $employeesRepository,
+        private ?EmailGreeter $emailGreeter = null
+    )
     {
     }
 
     public function sendGreetings(XDate $xDate, string $smtpHost, int $smtpPort): void
     {
         $employees = $this->employeesRepository->onBirthday($xDate);
-
-        $this->greeter = new EmailGreeter($smtpHost, $smtpPort);
 
         foreach ($employees as $employee) {
             $this->greeter->greet($employee);
